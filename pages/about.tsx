bbaf6196f602cd44f5d5cbef1985d49ca81a2e10
Abhimanyu R B <abhimanyurbsa@gmail.com>
2024-04-19 22:49:28 +0530

    Update about.tsx
    Fixed issue with testimonials

--- pages/about.tsx
+++ pages/about.tsx
@@ -40,14 +40,13 @@ const AboutPage = () => {
   ]
 
   const testimonials = [
-    "./assets/images/kochi.jpg",
-    "url_to_image2.jpg",
-    "url_to_image3.jpg",
-    "url_to_image4.jpg",
-    "url_to_image5.jpg",
+    { image: "./assets/images/kochi.jpg" },
+    { image: "url_to_image2.jpg" },
+    { image: "url_to_image3.jpg" },
+    { image: "url_to_image4.jpg" },
+    { image: "url_to_image5.jpg" },
   ];
   
-
   return (
     <div className="flex flex-col overflow-x-hidden">
       <NavBar />
